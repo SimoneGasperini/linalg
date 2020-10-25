fc9381378a88abfc795c62880c86c02bb269a0f8
SimoneGasperini <simone.gasperini2@studio.unibo.it>
2020-10-25 18:31:16 +0100

    added comments for clarity

--- linalg.hpp
+++ linalg.hpp
@@ -11,15 +11,15 @@ class Polynomial {
 
     public:
     Polynomial () {};
-    Polynomial (int);
-    Polynomial (int, double*);
-    Polynomial (const Polynomial&);
+    Polynomial (int degree);
+    Polynomial (int degree, double* coefficients);
+    Polynomial (const Polynomial& original);
     ~Polynomial ();
     int GetDegree () const;
     double* GetCoefficients () const;
-    void SetCoefficient (int, double);
-    double Evaluate (double);
-    double EvaluateDerivative (double);
+    void SetCoefficient (int index, double value);
+    double Evaluate (double point);
+    double EvaluateDerivative (double point);
     double ComputeZero ();
     double* ComputeZeros ();
 
@@ -38,26 +38,26 @@ class Vector {
 
     public:
     Vector () {};
-    Vector (int);
-    Vector (int, double*);
-    Vector (const Vector&);
+    Vector (int size);
+    Vector (int size, double* array);
+    Vector (const Vector& original);
     ~Vector ();
     int GetSize () const;
     double* GetArray () const;
-    double GetElement (int) const;
-    void SetElement (int, double);
-    double Norm (int p = 2);
-    Vector Normalized (int p = 2);
-    Vector ProjectedOnto (Vector);
+    double GetElement (int index) const;
+    void SetElement (int index, double value);
+    double Norm (int p=2);
+    Vector Normalized (int p=2);
+    Vector ProjectedOnto (Vector axis);
 
     friend ostream& operator << (ostream&, Vector&);
     friend istream& operator >> (istream&, Vector&);
     Vector& operator = (const Vector&);
     Vector operator + (const Vector&);
     Vector operator - (const Vector&);
-    double operator * (const Vector&);
-    Vector operator * (double);
-    Vector operator / (double);
+    double operator * (const Vector&); //dot product
+    Vector operator * (double scalar);
+    Vector operator / (double scalar);
     bool operator == (const Vector&);
     bool operator != (const Vector&);
 };
@@ -76,30 +76,30 @@ class Matrix {
 
     public:
     Matrix () {};
-    Matrix (int, int);
-    Matrix (int, int, double**);
-    Matrix (const Matrix&);
+    Matrix (int rows, int cols);
+    Matrix (int rows, int cols, double** matrix);
+    Matrix (const Matrix& original);
     ~Matrix ();
     int GetRows () const;
     int GetCols () const;
-    Vector GetRowVector (int) const;
-    Vector GetColVector (int) const;
-    double GetElement (int, int) const;
-    void SetElement (int, int, double);
-    double Norm (char p = 'f');
+    Vector GetRowVector (int index) const;
+    Vector GetColVector (int index) const;
+    double GetElement (int row_index, int col_index) const;
+    void SetElement (int row_index, int col_index, double value);
+    double Norm (char p='f');
     Matrix T ();
     Matrix I ();
     Matrix Triu ();
     Matrix Gauss ();
     int Rank ();
     double Trace ();
-    double Determinant (char method = 'g');
+    double Determinant (char method='g');
     Matrix* Eigendecomposition ();
     Matrix* SVdecomposition ();
     Matrix* QRdecomposition ();
-    Matrix* Choleskydecomposition (bool diag = false);
-    Matrix* LUdecomposition (bool diag = false);
-    Matrix HouseholderReflection (int);
+    Matrix* Choleskydecomposition (bool return_diag=false);
+    Matrix* LUdecomposition (bool return_diag=false);
+    Matrix HouseholderReflection (int index);
     Polynomial CharacteristicPol ();
     bool IsSquare ();
     bool IsSymmetric ();
@@ -110,23 +110,30 @@ class Matrix {
     Matrix& operator = (const Matrix&);
     Matrix operator + (const Matrix&);
     Matrix operator - (const Matrix&);
-    Matrix operator * (const Matrix&);
-    Matrix operator * (double);
-    Matrix operator / (double);
+    Matrix operator * (const Matrix&); //matrix multiplication
+    Matrix operator * (double scalar);
+    Matrix operator / (double scalar);
     bool operator == (const Matrix&);
     bool operator != (const Matrix&);
 };
 
-Matrix Eye (int);
-Vector Diag (Matrix);
-Matrix Diag (Vector);
-double Dot (Vector, Vector);
-Vector Dot (Matrix, Vector);
-Vector Dot (Vector, Matrix);
-Matrix Dot (Matrix, Matrix);
-Matrix Outer (Vector, Vector);
-Vector Hadamard (Vector, Vector);
-Vector* GramSchmidt (Vector*, int, int p = 2);
-double WilkinsonShift (Matrix);
-Vector Convolution (Vector, Vector);
-Matrix Convolution (Matrix, Matrix);
+double random_double (double min=0, double max=1);
+int random_int (int min=0, int max=1);
+
+Matrix Zeros (int* shape);
+Matrix Ones (int* shape);
+Matrix Rand (int* shape, double low=0, double high=1);
+Matrix RandInt (int* shape, int low=0, int high=1);
+Matrix Eye (int dim);
+Vector Diag (Matrix matrix);
+Matrix Diag (Vector diagonal);
+double Dot (Vector vec1, Vector vec2); //dot product (see Vector::*)
+Vector Dot (Matrix mat, Vector vec); //dot product
+Vector Dot (Vector vec, Matrix mat); //dot product
+Matrix Dot (Matrix mat1, Matrix mat2); //matrix multiplication (see Matrix::*)
+Matrix Outer (Vector vec1, Vector vec2); //outer product
+Vector Hadamard (Vector vec1, Vector vec2); //element-wise product
+Vector* GramSchmidt (Vector* vectors, int vectors_number, int p=2); //orthonormalization
+double WilkinsonShift (Matrix mat);
+Vector Convolution (Vector vec1, Vector vec2); //1-dimensional
+Matrix Convolution (Matrix mat1, Matrix mat2); //2-dimensional
